e5ead1a156fa7e152498a1cf1409304a92a1cc60
Sejal chourasia <sejalchourasia2000@gmail.com>
2020-04-22 01:26:53 +0530

    Added my project

---
+++ venv/TravelPlanner-master/travel_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Season,Summer,Winter,Autumn 
+# Register your models here.
+admin.site.register(Season)
+admin.site.register(Summer)
+admin.site.register(Winter)
+admin.site.register(Autumn)
---
+++ venv/TravelPlanner-master/travel_app/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path     
+from . import views
+urlpatterns = [
+    
+    path('', views.index, name="index"),
+    path('register', views.register_page),
+    path('login', views.login_page),
+    path('register/process', views.register),
+    path('login/process', views.login),
+    path('about', views.about),
+    path('contact', views.contact),
+    path('summer', views.summer),
+    path('winter', views.winter),
+    path('autumn', views.autumn),
+    path('', views.elements, name="elements"),
+    path('', views.news, name="news"),
+    path('', views.homepage),
+    path('dashboard', views.dashboard),	 
+    path('logout', views.logout),
+    path('trip/add', views.add_trip_page),
+    path('add_trip', views.add_trip),  
+    path('trip/edit/<int:num>', views.edit_trip_page),
+    path('trip/edit/<int:num>/process', views.edit_trip),
+    path('trip/<int:num>', views.trip_info_page),
+    path('delete/<int:num>', views.delete_trip)
+    
+]
